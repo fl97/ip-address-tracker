93044b7a6b07ebb56bbaf2617cca5f359fe450e2
fl97 <50992060+fl97@users.noreply.github.com>
2021-07-22 12:47:50 +0200

    updated js

--- main.js
+++ main.js
@@ -1,53 +1,90 @@
-const ipAddress = document.getElementById("ip-address");
-const ipLocation = document.getElementById("location");
-const timezone = document.getElementById("timezone");
-const isp = document.getElementById("isp");
-const input = document.querySelector(".search-form");
-var map = L.map('map');
+const inputField = document.querySelector(".text-input")
+const submitBtn = document.querySelector(".button")
+const ipData = document.querySelector("#ip-address")
+const locationData = document.querySelector("#location")
+const timezoneData = document.querySelector("#timezone")
+const ispData = document.querySelector("#isp")
+const mapContainer = document.querySelector("#map")
+const map = L.map(mapContainer).setView([0, 0], 15)
+const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const attribution = '&copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+const tiles = L.tileLayer(tileURL, { attribution }).addTo(map)
+const locationMarker = L.icon({
+    iconUrl: "/images/icon-location.svg",
+    iconSize: [46, 56],
+})
 
-let lat;
-let lng;
+displayClientIP()
 
-const displayMap = () => {
-    var markerIcon = L.icon({
-        iconUrl: 'images/icon-location.svg',
+submitBtn.addEventListener("click", e => {
+    e.preventDefault()
 
-        iconSize: [46, 56], // size of the icon
-        iconAnchor: [23, 55], // point of the icon which will correspond to marker's location
-    });
-    map.setView([lat, lng], 17);
+    if (inputField.value) {
+        checkData(inputField.value)
+    } else {
+        displayError()
+    }
+})
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: false
-    }).addTo(map);
+//displays the Client IP on load
+async function displayClientIP() {
+    const response = await fetch("https://geo.ipify.org/api/v1?apiKey=at_1UOKYLUYX6wX01ZGYWP76R8ncaxAh")
+    const data = await response.json()
+    const clientIP = await data.ip
 
-    L.marker([lat, lng], { icon: markerIcon }).addTo(map)
+    inputField.value = clientIP
+    checkData(clientIP)
+}
 
-};
+function checkData(inputValue) {
+    const input = inputValue
+    const domainFormat = /^(?!\-)([a-zA-Z0-9 \-?]{2,63})\.([a-zA-Z]{2,4})(.[a-z]{2,4})?$/
+    const ipFormat = /^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/
 
-const displayIpInfos = (data) => {
-    ipAddress.innerText = data.ip;
-    ipLocation.innerText = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
-    timezone.innerText = `UTC ${data.location.timezone}`;
-    console.log(data.location)
-    isp.innerText = data.isp;
-};
+    if (input.match(domainFormat)) {
+        fetchData(input, "domain")
+    } else if (input.match(ipFormat)) {
+        fetchData(input, "ipAddress")
+    } else {
+        alert("invalid input")
+    }
+}
 
-const getIpInfos = (ipAddress = "") => {
-    fetch(`https://geo.ipify.org/api/v1?apiKey=at_1UOKYLUYX6wX01ZGYWP76R8ncaxAh&ipAddress=${ipAddress}`)
-        .then(response => response.json())
-        .then(data => {
-            lat = data.location.lat;
-            lng = data.location.lng;
-            displayIpInfos(data);
-            displayMap();
-        })
-};
+async function fetchData(inputValue, inputType) {
+    const url = "https://geo.ipify.org/api/v1?"
+    const API_Key = "at_1UOKYLUYX6wX01ZGYWP76R8ncaxAh"
+    const urlTemplate = `${url}apiKey=${API_Key}&${inputType}=${inputValue}`
 
-getIpInfos();
+    const response = await fetch(`${urlTemplate}`)
+    const data = await response.json()
 
-input.addEventListener("submit", event => {
-    event.preventDefault()
-    getIpInfos(event.target[0].value);
-    event.target[0].value = "";
-});
+    generateInfo(data)
+    generateMap(data)
+
+    inputField.value = ""
+}
+
+function generateInfo(info) {
+    const ip = info.ip
+    const isp = info.isp
+    const location = `${info.location.city}, ${info.location.country} ${info.location.postalCode}`
+    const timezone = info.location.timezone
+
+    ipData.textContent = ip
+    locationData.textContent = location
+    timezoneData.textContent = "UTC " + timezone
+    ispData.textContent = isp
+}
+
+function generateMap(info) {
+    const lat = info.location.lat
+    const long = info.location.lng
+    const location = `${info.location.city}, ${info.location.country} ${info.location.postalCode}`
+
+    const marker = L.marker([lat, long], { icon: locationMarker }).addTo(map)
+
+    map.panTo([lat, long])
+    marker.bindPopup(location)
+}
+
+const displayError = () => alert("Please enter IP Address or domain")
